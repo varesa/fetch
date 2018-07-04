f50abb0fbb9f0d8d96ec7f72678b89b2c8320b49
Esa Varemo <esa@kuivanto.fi>
2018-07-04 03:29:50 +0300

    Try downloading files

--- run.sh
+++ run.sh
@@ -9,8 +9,19 @@ export LD_PRELOAD=libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
 
+HOME=/tmp/home
+mkdir -p ${HOME}
+
+WORK=$HOME/work
+mkdir -p ${WORK}
+
 while true
 do
     ssh -o StrictHostKeyChecking=no -i /keys/ssh/fetch ${REMOTE_USER}@${REMOTE_HOST} "ls -lah ${REMOTE_PATH}"
+    scp -o StrictHostKeyChecking=no -i /keys/ssh/fetch ${REMOTE_USER}@${REMOTE_HOST} "${REMOTE_PATH}/*" ${WORK}
+
+    for file in ${WORK}/*; do
+        echo $file
+    done
     sleep 5
 done
