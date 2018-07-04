ebf87fafe1329d3d86f27c1df0c55072883653e7
Esa Varemo <esa@kuivanto.fi>
2018-07-04 03:11:26 +0300

    Fix passwd.template path

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
-envsubst < ${HOME}/passwd.template > /tmp/passwd
+envsubst < passwd.template > /tmp/passwd
 export LD_PRELOAD=libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
