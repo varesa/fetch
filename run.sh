1da94af477be1a7e0ede992fdfeecbcdee811599
Esa Varemo <esa@kuivanto.fi>
2019-05-08 13:21:48 +0300

    Improve script output and don't crash/exit if no files are available

--- run.sh
+++ run.sh
@@ -17,6 +17,10 @@ export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
 
+function log {
+    echo "$(date -Iseconds) $@"
+}
+
 #
 # GPG Init
 #
@@ -42,18 +46,27 @@ REMOTE="${REMOTE_USER}@${REMOTE_HOST}"
 WORK=/tmp/work
 mkdir -p ${WORK}
 
+last_miss_hour=""
+
 while true
 do
-    # Test the connection
-    ssh ${SSH_OPTS} ${REMOTE} "true"
-
-    # Check and download any queued files
-    if scp ${SSH_OPTS} "${REMOTE}:${REMOTE_PATH}/*" ${WORK}; then
+    # Check if there are files
+    if [[ -z "$(ssh ${SSH_OPTS} ${REMOTE} \"find ${REMOTE_PATH}/ -type f\")" ]]; then
+        # No files to be found, write a log message once per hour
+        hour="$(date +%H)"
+        if [[ "$hour" != "$last_miss_hour" ]];
+            log "No files found (message silenced for this hour)"
+        fi
+        last_miss_hour="$hour"
+    else
+        log "Downloading queued files"
+        scp ${SSH_OPTS} "${REMOTE}:${REMOTE_PATH}/*" ${WORK}; then
 
         for file in ${WORK}/*; do
             # Archive all the files
             cp ${file} /archive/
 
+            log "Processing ${file}"
             process_file ${file}
 
             # Make sure that everything is fine so far and we have a file
@@ -65,12 +78,10 @@ do
                 # ... and remotely
                 ssh ${SSH_OPTS} ${REMOTE} "rm ${REMOTE_PATH}/$(basename ${file})"
             else
-                echo "Something went wrong and we didn't get a file"
+                log "Something went wrong and we didn't get a file"
                 exit 1
             fi
         done
-    else
-        echo "No files found"
     fi
 
     sleep 60
